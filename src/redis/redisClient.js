f549a8eff5ec6037d6bf0e706cd26c0f5b75f264
Prajna1999 <gituprajna20@gmail.com>
2024-09-24 23:42:33 +0530

    ADD: extra methods to redis client

--- src/redis/redisClient.js
+++ src/redis/redisClient.js
@@ -11,7 +11,7 @@ class RedisClient {
         this.client = null;
         this.port = port;
         this.host = host;
-        // this.options = options
+        this.options = options;
     }
     async connect() {
         try {
@@ -33,7 +33,73 @@ class RedisClient {
             throw error;
         }
     }
+    async disconnect() {
+        try {
+            await this.client.quit();
+            logger.info('Redis client disconnected');
+        } catch (error) {
+            logger.error('Failed to disconnect Redis', error);
+            throw error;
+        }
+    }
+    async set(key, value, expirationInSeconds = null) {
+        try {
+            const serlizedValue = JSON.stringify(value);
+            if (expirationInSeconds) {
+                await this.client.setEx(key, expirationInSeconds, serlizedValue);
 
+            } else {
+                await this.client.set(key, serlizedValue);
+
+            }
+            logger.info(`Successfully set key: ${key}`);
+        } catch (error) {
+            logger.error(`Error setting data in Redis for key: ${key}`, error);
+            throw error;
+        }
+    }
+    async get(key) {
+        try {
+            const deserialisedValue = await this.client.get(key);
+            return value ? JSON.parse(value) : null;
+
+        } catch (error) {
+            logger.error(`Error getting the data from Redis for key: ${key}`, error);
+            throw error;
+        }
+    }
+
+    async delete(key) {
+        try {
+            await this.client.del(key);
+            logger.info(`Successfully deleted key: ${key}`);
+
+        } catch (error) {
+            logger.error(`Error in deleting the key: ${key}`, error);
+            throw error;
+        }
+    }
+
+    async exists(key) {
+        try {
+            return await this.client.exists(key);
+
+        } catch (error) {
+            logger.err(`Error checking the existence of Redis for key: ${key}`, error);
+            throw error;
+        }
+    }
+
+    async ping() {
+        try {
+            const response = await this.client.ping();
+            logger.info('Redis Ping Response:', response);
+            return response;
+        } catch (error) {
+            logger.error('Error pinging Redis:', error);
+            throw error;
+        }
+    }
 }
 const redisClient = new RedisClient();
 module.exports = redisClient;
